dde3610d953496374fd04e7c171c86441ce6e925
FA12-BSE-133 <FA12-BSE-133@LAB7C-07.cns.ciit>
2015-11-09 10:07:04 +0500

    Hai tmharay hawalay mutton saathiyo

---
+++ GIT/src/git.java
@@ -0,0 +1,11 @@
+
+public class git {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Lets do some GIT");
+		System.out.println("Dhinka chikaa dhinka chikaa ");
+
+	}
+
+}
