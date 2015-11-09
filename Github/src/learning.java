590ab958357f4a9c1deeb14a6a03e5b9e3484859
FA12-BSE-133 <FA12-BSE-133@LAB7C-07.cns.ciit>
2015-11-09 11:15:50 +0500

    1st comment hai

--- Github/src/learning.java
+++ Github/src/learning.java
@@ -4,7 +4,7 @@ public class learning {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("1st Comment");
-System.out.println("2nd Comment");
+
 	}
 
 }
